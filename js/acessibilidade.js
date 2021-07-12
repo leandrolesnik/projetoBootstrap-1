f81cbe9696f5092514456257c50ee3b4cd82bc31
Bruno Bergamaschi <brunobergamaschi@live.com>
2021-07-12 18:27:21 -0300

    primeiro commit

---
+++ js/acessibilidade.js
@@ -0,0 +1,11 @@
+let fontSize = 16;
+
+let diminuir = () => {
+   fontSize--;
+   document.body.style.fontSize = fontSize + "px";
+}
+
+let aumentar = () => {
+   fontSize++;
+   document.body.style.fontSize = fontSize + "px";
+}
