c81b387fadeece5183b53337d2148df07c723eb4
Christina Fernandes <christina.o.fernandes21@gmail.com>
2021-07-13 15:12:14 -0300

    consertei errinhos, coloquei imagens e textos

---
+++ js/whatsapp.js
@@ -0,0 +1,28 @@
+const formulario = document.querySelector('#formulario');
+const buttonSubmit = document.querySelector('#submit');
+const urlDesktop = 'https://api.whatsapp.com/';
+const telefone = '88999053714';
+
+  const whatsapp = () => {
+     let nome = document.querySelector('#nome').value;
+     let sobrenome = document.querySelector('#sobrenome').value;
+     let email = document.querySelector('#email').value;
+     let idade = document.querySelector('#idade').value;
+     let cpf = document.querySelector('#cpf').value;
+     let genero = document.querySelector('#selectGenero').value;
+     let acompanhamento; 
+     if (document.querySelector('#inputSimAcompanhamento').checked = true)
+        acompanhamento = document.querySelector('#infoAcompanhamento').value;
+     else if(document.querySelector('#inputNaoAcompanhamento').checked = true)
+        acompanhamento = "Não";
+
+     let comorbidade; 
+     if (document.querySelector('#inputSimComorbidade').checked = true)
+        comorbidade = document.querySelector('#infoComorbidade').value;
+     else if (document.querySelector('#inputNaoComorbidade').checked = true)
+        comorbidade = "Não";
+
+     let mensagem = "send?phone=" + telefone + "&text=* Formulario Contato de cliente -->> %0A Nome: %0A " + nome + " %0A Sobrenome:%0A" + sobrenome + "%0A E-mail: %0A "+ email +" %0A Idade: %0A "+ idade +" %0A Genero: %0A" + genero +"%0A Acompanhamento: %0A" + acompanhamento +"%0A Comorbidade: %0A" + comorbidade;
+
+     window.open(urlDesktop + mensagem, '_blank')
+  }
